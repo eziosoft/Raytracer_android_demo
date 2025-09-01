fe2394446622334bbc51842b439f4f811b943b86
bartosz.szczygiel <bartosz.szczygiel@reakto.eu>
2025-09-01 12:53:15 +0200

    disable AI usage in game settings

--- app/src/main/java/com/example/fps_raytrace/engine/Const.kt
+++ app/src/main/java/com/example/fps_raytrace/engine/Const.kt
@@ -19,6 +19,6 @@ object Const {
     val PLAYER_ROTATION_SPEED_RAD = 2f.toRadian()
     val PLAYER_FOV = 100.toRadian()
 
-    const val USE_AI = true
+    const val USE_AI = false
     const val AI_RECORD_DATA = false
 }
